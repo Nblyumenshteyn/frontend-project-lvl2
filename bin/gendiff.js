ef25751ef910c9506f35b5a1da051effe1ae5e20
Natalya Blyumenshteyn <natalya.blyumenshteyn@dualbootpartners.com>
2022-04-15 17:22:00 +0400

    task #1

---
+++ bin/gendiff.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
+
+// program.command('gendiff')
+//   .description('Compares two configuration files and shows a difference')
+//   .argument('<string>', 'string to split')
+//   .option('--first', 'display just the first substring')
+//   .option('-s, --separator <char>', 'separator character', ',')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+program.parse();
+
+
+
+
+
+// Options:
+//   -V, --version        output the version number
+//   -h, --help           display help for command
