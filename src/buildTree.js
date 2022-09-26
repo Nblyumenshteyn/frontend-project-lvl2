d4770fd4dd5136ff634ef761b501e87c9fd3d656
Natalya Blyumenshteyn <natalya.blyumenshteyn@dualbootpartners.com>
2022-09-26 15:26:02 +0400

    refactoring

--- src/buildTree.js
+++ src/buildTree.js
@@ -9,7 +9,6 @@ const buildTree = (file1, file2, format) => {
     const arr1 = Object.keys(file1);
     const arr2 = Object.keys(file2);
     const commonKeys = Array.from(new Set([...arr1, ...arr2])).sort();
-    // const commonKeys = Object.keys({ ...file1, ...file2 });
 
     level += 1;
 
@@ -20,76 +19,52 @@ const buildTree = (file1, file2, format) => {
       if (!arr1.includes(key) && arr2.includes(key)) {
         if (isObject(second)) {
           acc.push({
-            level, key, type: 'added', valueAfter: second,  valueAfterObj: true,  isLast: false,
+            level, key, type: 'added', valueAfter: second, valueAfterObj: true, isLast: false,
           });
         } else {
           acc.push({
             level, key, type: 'added', valueAfter: second, valueAfterObj: false, isLast: false,
-          })
+          });
         }
       } else if (arr1.includes(key) && !arr2.includes(key)) {
         if (isObject(first)) {
           acc.push({
-            level, key, type: 'removed', valueBefore: first,  valueBeforeObj: true, isLast: false,
+            level, key, type: 'removed', valueBefore: first, valueBeforeObj: true, isLast: false,
           });
         } else {
           acc.push({
             level, key, type: 'removed', valueBefore: first, valueBeforeObj: false, isLast: false,
-          })
+          });
         }
       } else if (arr1.includes(key) && arr2.includes(key) && isObject(first) && isObject(second)) {
-        acc.push({ level, key, type: 'modifiedChild',  isLast: false,});
+        acc.push({
+          level, key, type: 'modifiedChild', isLast: false,
+        });
         analytheTree(first, second, acc, level);
       } else if (arr1.includes(key) && arr2.includes(key) && first !== second && isObject(first)) {
         acc.push({
-          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: true, valueAfterObj: false,  isLast: false,
+          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: true, valueAfterObj: false, isLast: false,
         });
       } else if (arr1.includes(key) && arr2.includes(key) && first !== second && isObject(second)) {
         acc.push({
-          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: false, valueAfterObj: true,  isLast: false,
+          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: false, valueAfterObj: true, isLast: false,
         });
       } else if (arr1.includes(key) && arr2.includes(key) && first !== second) {
         acc.push({
-          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: false, valueAfterObj: false,  isLast: false,
+          level, key, type: 'updated', valueBefore: first, valueAfter: second, valueBeforeObj: false, valueAfterObj: false, isLast: false,
         });
       } else {
         acc.push({
-          level, key, type: 'unchanged', valueBefore: first, valueAfter: second,  isLast: false,
+          level, key, type: 'unchanged', valueBefore: first, valueAfter: second, isLast: false,
         });
       }
-
-      // if (!arr1.includes(key) && arr2.includes(key)) {
-      //   acc.push({
-      //     level, key, type: 'added', valueAfter: second,
-      //   });
-      // } else if (arr1.includes(key) && !arr2.includes(key)) {
-      //   acc.push({
-      //     level, key, type: 'deleted', valueBefore: first,
-      //   });
-      // } else if (arr1.includes(key) && arr2.includes(key) && isObject(first) && isObject(second)) {
-      //   acc.push({ level, key, type: 'modifiedChild'});
-      //   analytheTree(first, second, acc, level);
-      // } else if (arr1.includes(key) && arr2.includes(key) && first !== second) {
-      //   acc.push({
-      //     level, key, type: 'modified', valueBefore: first, valueAfter: second,
-      //   });
-      // } else {
-      //   acc.push({
-      //     level, key, type: 'unchanged', valueBefore: first, valueAfter: second,
-      //   });
-      // }
     });
 
-if(acc.length > 0 && level > 0){
-    // if(acc[acc.length - 1].type  ==='unchanged' || acc[acc.length - 1].type  ==='added' || acc[acc.length - 1].type === 'deleted' || acc[acc.length - 1].type === 'modified')
-    {
+    if (acc.length > 0 && level > 0) {
       const isLargeNumber = (element) => element.level === level;
-      var foundIndex = acc.findLastIndex(isLargeNumber);
+      const foundIndex = acc.findLastIndex(isLargeNumber);
       acc[foundIndex].isLast = true;
-      //acc[acc.length - 1].isLast = true;
-      // acc.pop().isLast = true;
     }
-  }
     level -= 1;
     return acc;
   };
@@ -98,7 +73,6 @@ if(acc.length > 0 && level > 0){
   const level = -1;
 
   analytheTree(file1, file2, acc, level);
-  // console.log(acc);
   const result = formatter(acc, format);
   return result;
 };
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,36 +1,27 @@
 const stylish = (tree) => {
-
-
   const closeBrackets = (node, indent, tempNodeList) => {
-    if(node.isLast) {
+    if (node.isLast) {
+      let str = `\n${indent.substring(0, indent.length - 2)}}`;
 
-      var str = `\n${indent.substring(0, indent.length - 2)}}`;
+      let uplevel = 1;
 
-      var uplevel = 1;
-
-      if(tempNodeList.length > 0){
-      for(var i = tempNodeList.length - 1; i >= 0; i--) {
-        if(tempNodeList[i].level === node.level - uplevel && tempNodeList[i].isLast) {
-          str = `${str}\n${indent.substring(0, indent.length - 2 - (4 * uplevel))}}`;
-          uplevel++;
+      if (tempNodeList.length > 0) {
+        for (let i = tempNodeList.length - 1; i >= 0; i -= 1) {
+          if (tempNodeList[i].level === node.level - uplevel && tempNodeList[i].isLast) {
+            str = `${str}\n${indent.substring(0, indent.length - 2 - (4 * uplevel))}}`;
+            uplevel += 1;
+          }
         }
-        // else if(tempNodeList[i].level === node.level + 1)
-        // {
-        //   return str;
-        // }
-    }
-    }
+      }
 
       return str;
-    } else {
-      return '';
     }
+    return '';
   };
 
   const tempNodeList = [];
 
   const result = tree.map((node) => {
-
     tempNodeList.push(node);
 
     const indent = '    '.repeat(node.level);
@@ -47,25 +38,22 @@ const stylish = (tree) => {
       case 'updated': {
         if (node.valueBeforeObj === true) {
           return `${indent}- ${node.key}: ${valueBefore}\n${indent}+ ${node.key}: ${node.valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
-        } else if (node.valueAfterObj === true) {
+        } if (node.valueAfterObj === true) {
           return `${indent}- ${node.key}: ${node.valueBefore}\n${indent}+ ${node.key}: ${valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
-        } else {
-          return `${indent}- ${node.key}: ${node.valueBefore}\n${indent}+ ${node.key}: ${node.valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
         }
+        return `${indent}- ${node.key}: ${node.valueBefore}\n${indent}+ ${node.key}: ${node.valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
       }
       case 'added': {
         if (node.valueAfterObj === true) {
           return `${indent}+ ${node.key}: ${valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
-        } else {
-          return `${indent}+ ${node.key}: ${node.valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
         }
+        return `${indent}+ ${node.key}: ${node.valueAfter}${closeBrackets(node, indent, tempNodeList)}`;
       }
       case 'removed': {
         if (node.valueBeforeObj === true) {
           return `${indent}- ${node.key}: ${valueBefore}${closeBrackets(node, indent, tempNodeList)}`;
-        } else {
-          return `${indent}- ${node.key}: ${node.valueBefore}${closeBrackets(node, indent, tempNodeList)}`;
         }
+        return `${indent}- ${node.key}: ${node.valueBefore}${closeBrackets(node, indent, tempNodeList)}`;
       }
       default: {
         throw Error('There is no such type');
